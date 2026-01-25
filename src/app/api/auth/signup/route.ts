03e5ced81f986e7b903b950c75007983f021c15e
Munazza Javed <munazzajaved277@gmail.com>
2026-01-25 18:14:43 +0500

    made changes

--- src/app/api/auth/signup/route.ts
+++ src/app/api/auth/signup/route.ts
@@ -7,14 +7,16 @@ import User from '@/models/User';
 export async function POST(request: NextRequest) {
   try {
     await connectToDatabase();
-    const { name, email, password, role } = await request.json();
-    if (!name || !email || !password || !role) {
+    const { name, email, password } = await request.json();
+    if (!name || !email || !password) {
       return NextResponse.json({ error: 'All fields are required' }, { status: 400 });
     }
     const existingUser = await User.findOne({ email });
     if (existingUser) {
       return NextResponse.json({ error: 'User already exists' }, { status: 400 });
     }
+    const usersCount = await User.countDocuments();
+    const role = usersCount === 0 ? 'admin' : 'user';
     const hashedPassword = await bcrypt.hash(password, 10);
     const user = new User({ name, email, password: hashedPassword, role });
     await user.save();
